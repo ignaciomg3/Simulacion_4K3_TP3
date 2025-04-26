c5b4908dc7d5abb46ae7ec8bfe851859fa98efe4
Conrado <conryjuncon@hotmail.com>
2025-04-26 19:42:27 -0300

    Columnas montecarlo

---
+++ tp3.py
@@ -0,0 +1,17 @@
+"""
+Columnas de Montecarlo
+
+| Dia (Obligatorio)
+| Obreros totales (Hardcoded, 21 22 23 24)
+| RND obreros ausentes 
+| Obreros ausentes 
+| Obreros restantes 
+| Fabrica en funcionamiento 
+| Venta productos (Opcional, default $4000)
+| Costo produccion (Opcional, default $2400)
+| Costo por obrero (Opcional, default $30)
+| Ganancia del día 
+| Ganancia AC  
+
+"""
+
